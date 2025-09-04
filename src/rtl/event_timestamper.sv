5670854295561ed84ca66bb5fdb74daf5c9f5a49
zeeeli <zee578995@gmail.com>
2025-09-03 23:17:44 -0700

    Added port list and free running counter

---
+++ src/rtl/event_timestamper.sv
@@ -0,0 +1,38 @@
+module event_timestamper #(
+    parameter int unsigned ID_W = 4,      // Width of event IDs
+    parameter int unsigned TS_W = 64      // Width of timestamp counter
+) (
+    input logic clk,
+    input logic rst,
+
+    // Event Start
+    input  logic            start_valid,  // Pulse: High while start is pending
+    output logic            start_ready,  // Hold:  Unit is ready to start counting
+    input  logic [ID_W-1:0] start_id,     // Hold:  ID of event
+
+    // Event End
+    input  logic            end_valid,    // Pulse: High while timestamp end is pending
+    output logic            end_ready,    // Hold:  Unit is ready to end counting
+    input  logic [ID_W-1:0] end_id,       // Hold:  ID of event (meant to match start)
+
+    // Output Record
+    output logic            out_valid,    // Pulse: High when results is ready
+    input  logic            out_ready,    // Hold:  Tells downstream it can get timestamp
+    output logic [ID_W-1:0] out_id,       // Hold:  ID of event
+    output logic [TS_W-1:0] out_start_ts, // Hold:  Timestamp caputred at the start
+    output logic [TS_W-1:0] out_end_ts,   // Hold:  Timestamp caputred at the end
+    output logic [TS_W-1:0] out_ts        // Hold:  True timestamp (end - start)
+);
+
+  //--------------------------
+  //Free Running Counter
+  //--------------------------
+  logic [TS_W-1:0] cnt_q;
+  always_ff @(posedge clk) begin : timestamp
+    if (rst) cnt_q <= '0;
+    else     cnt_q <= cnt_q + 1'b1;
+  end
+
+  // TODO: Scoreboard, handshakes, data pipelining, and outputs
+
+endmodule
