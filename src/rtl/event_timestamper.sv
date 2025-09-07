6261c72cf232483ea2ff54bf6890ea822e1a28cc
zeeeli <zee578995@gmail.com>
2025-09-06 23:47:34 -0700

    Some initial work on Scoreboard and handshaking

--- src/rtl/event_timestamper.sv
+++ src/rtl/event_timestamper.sv
@@ -35,4 +35,34 @@ module event_timestamper #(
 
   // TODO: Scoreboard, handshakes, data pipelining, and outputs
 
+  //--------------------------
+  // Scoreboard Storage
+  //--------------------------
+  // NOTE: Need to track start timestamps by id and ids in use
+  localparam int unsigned DEPTH = (2**ID_W);   // RAM is as deep as different id values
+
+  // Per-ID start timestamp
+  logic [TS_W-1:0] start_ts_mem [DEPTH];       // Stores the timestamp caputred at start of ID event
+  logic            valid_mem    [DEPTH];       // Flag: if valid_mem[id] == 1 then id is "active"
+
+  // Clearing RAM
+  always_ff @(posedge clk) begin : valid_mem_clear
+    if (rst) begin
+      for (int i = 0; i < DEPTH; i++) begin
+        valid_mem[i] <= '0;
+      end
+    end
+  end
+
+  //--------------------------
+  // Handshakes and Hazards
+  //--------------------------
+  // Current scoreboard state @ ID request
+  logic valid_at_start, valid_at_end;
+  assign valid_at_start = valid_mem[start_id];    // Check if ID already in flight
+  assign valid_at_end   = valid_mem[end_id];      // Check if ID has already started
+
+  // Same-cycle and Same-ID Hazard
+  logic hazard_same_id;
+  assign hazard_same_id = start_valid && end_valid && (start_id == end_id);
 endmodule
