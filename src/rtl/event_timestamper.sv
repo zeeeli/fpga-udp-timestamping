ff94a789e153d19fb7d5cddba108391ff49cb614
zeeeli <zee578995@gmail.com>
2025-09-09 20:08:30 -0700

    Added minimal handshaking and scoreboard writing

--- src/rtl/event_timestamper.sv
+++ src/rtl/event_timestamper.sv
@@ -24,9 +24,9 @@ module event_timestamper #(
     output logic [TS_W-1:0] out_ts        // Hold:  True timestamp (end - start)
 );
 
-  //--------------------------
+  //--------------------------------------------------------------------------------------------------------
   //Free Running Counter
-  //--------------------------
+  //--------------------------------------------------------------------------------------------------------
   logic [TS_W-1:0] cnt_q;
   always_ff @(posedge clk) begin : timestamp
     if (rst) cnt_q <= '0;
@@ -35,9 +35,9 @@ module event_timestamper #(
 
   // TODO: Scoreboard, handshakes, data pipelining, and outputs
 
-  //--------------------------
+  //--------------------------------------------------------------------------------------------------------
   // Scoreboard Storage
-  //--------------------------
+  //--------------------------------------------------------------------------------------------------------
   // NOTE: Need to track start timestamps by id and ids in use
   localparam int unsigned DEPTH = (2**ID_W);   // RAM is as deep as different id values
 
@@ -54,9 +54,9 @@ module event_timestamper #(
     end
   end
 
-  //--------------------------
+  //--------------------------------------------------------------------------------------------------------
   // Handshakes and Hazards
-  //--------------------------
+  //--------------------------------------------------------------------------------------------------------
   // Current scoreboard state @ ID request
   logic valid_at_start, valid_at_end;
   assign valid_at_start = valid_mem[start_id];    // Check if ID already in flight
@@ -65,4 +65,29 @@ module event_timestamper #(
   // Same-cycle and Same-ID Hazard
   logic hazard_same_id;
   assign hazard_same_id = start_valid && end_valid && (start_id == end_id);
+
+  // NOTE: Minimal Handshake
+  // end_ready   -> requires the ID to be active AND destination to be ready
+  // start_ready -> blocks the reuse of an active ID and blocks same ID
+  //                when END fires
+  logic end_may_fire;
+  assign end_ready    = valid_at_end      && out_ready;
+  assign end_may_fire = end_valid         && end_ready;
+  assign start_ready  = (!valid_at_start) && !(hazard_same_id && end_may_fire);
+
+  // handshaking events (fires)
+  logic start_fire, end_fire;
+  assign start_fire = start_valid && start_ready;
+  assign end_fire   = end_valid   && end_ready;
+
+  //--------------------------------------------------------------------------------------------------------
+  // Writing Into Scoreboard
+  //--------------------------------------------------------------------------------------------------------
+  // On start fire, keep current timestamp and ID active
+  always_ff @(posedge clk) begin : write_to_scoreboard
+    if (start_fire) begin
+      start_ts_mem[start_id] <= cnt_q;
+      valid_mem[start_id]    <= 1'b1;
+    end
+  end
 endmodule
