f0e1c5be2e51d19535b55bc8fe1497d95b407d65
zeeeli <zee578995@gmail.com>
2025-10-10 20:35:36 -0700

    Changed func to task and added tests

--- tb/tb_top_uart_logger.sv
+++ tb/tb_top_uart_logger.sv
@@ -62,8 +62,8 @@ module tb_top_uart_logger;
   endtask
 
   // UART Rx to monitor tx line
-  function automatic byte recv_uart_byte ();
-    byte b;
+  task automatic recv_uart_byte(output byte b);
+    bit [7:0] data;
 
     // Wait for falling edge (start bit)
     @(negedge tx);
@@ -72,20 +72,55 @@ module tb_top_uart_logger;
     assert (tx==1'b0) else $error("Start bit not low at center");
     for (int i = 0; i < 8; i++) begin
       wait_cycles(BIT_CYCLES);
-      b[i] = tx; // LSB first
+      data[i] = tx; // LSB first
     end
     // Stop bit
     wait_cycles(BIT_CYCLES);
     assert (tx == 1'b1) else $error("Stop bit not high");
-    return b;
-  endfunction
+    b = data;
+  endtask
 
+  // concatenating the recieved bytes into an output string
   task automatic recv_n_bytes(
     output string s,
     input int n);
     s = "";
     for (int i = 0; i < n; i++) begin
-      s = {s, recv_uart_byte()};
+      byte tmp;
+      recv_uart_byte(tmp);
+      s = {s, tmp};
     end
   endtask
+
+  string recv, exp;
+  // Event Tests
+  initial begin
+    // default values
+    out_valid = 0; out_id = 0; out_start_ts = 0; out_end_ts = 0; out_delta = 0;
+
+    // reset
+    wait_cycles(6); rst = 1'b0;
+
+    // NOTE: Test Event 1
+
+    push_event(16'h0012, 64'h0123_4567_89AB_CDEF,
+               64'h0000_0000_0000_00A5, 64'h0000_0000_FEDC_BA98);
+    recv_n_bytes(recv, LINE_BYTES);
+    exp = "0012,0123456789ABCDEF,00000000000000A5,00000000FEDCBA98\n";
+    if (recv !== exp) begin
+      $display("FAIL E1:\n got: %s\n exp: %s", recv, exp); $fatal;
+    end else $display("PASS E1: %s", recv);
+
+    // NOTE: Event 2
+    push_event(16'hABCD, 64'hDEAD_BEEF_CAFE_BABE,
+               64'h0000_0000_0000_0001, 64'h1122_3344_5566_7788);
+    recv_n_bytes(recv, LINE_BYTES);
+    exp = "ABCD,DEADBEEFCAFEBABE,0000000000000001,1122334455667788\n";
+    if (recv !== exp) begin
+      $display("FAIL E2:\n got: %s\n exp: %s", recv, exp); $fatal;
+    end else $display("PASS E2: %s", recv);
+
+    $display("PASS: top-level logger+UART functional.");
+    wait_cycles(200); $finish;
+  end
 endmodule
